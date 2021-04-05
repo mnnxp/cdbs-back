79b8164732acf034b680d05751483a33ef094953
I_Nosovskiy <mananaxep@gmail.com>
2021-04-05 18:49:10 +0300

    add commented code for data deletion function

--- src/models/component/service/delete.rs
+++ src/models/component/service/delete.rs
@@ -1,6 +1,47 @@
 // use crate::database::{db_connection, Pool};
-// use crate::errors::{ServiceError, ServiceResult};
-// use crate::models::user::model::{SlimUser, User};
-// use crate::models::user::util::verify;
+// use crate::errors::{
+//     // ServiceError,
+//     ServiceResult
+// };
+// use crate::models::component::model::{
+//     SlimComponent,
+//     Component
+// };
+// // use crate::models::user::util::verify;
 // use actix_web::web;
 // use diesel::prelude::*;
+// use uuid::Uuid;
+//
+// pub fn delete(
+//     user_uuid: Uuid,
+//     component_delete: Uuid,
+//     pool: web::Data<Pool>
+// ) -> ServiceResult<SlimComponent> {
+//     let conn = &db_connection(&pool)?;
+//
+//     delete_component(user_uuid, component_delete, conn)
+// }
+//
+// fn delete_component(
+//     user_uuid: Uuid,
+//     component_delete: Uuid,
+//     conn: &PgConnection
+// ) -> ServiceResult<SlimComponent> {
+//     use crate::schema::component_ref::dsl::*;
+//
+//     // debug!("fn user_uuid = {}", &user_uuid);
+//     // debug!("fn component_delete = {}", &component_delete);
+//
+//     let delete_component: Component = component_ref
+//         .filter(uuid.eq(user_uuid))
+//         .filter(uuid.eq(component_delete))
+//         .get_result(conn)?;
+//
+//     diesel::delete(
+//         component_ref.filter(uuid.eq(delete_component.uuid))
+//     ).execute(conn)?;
+//
+//     // debug!("fn delete_component ={:?}", &delete_component);
+//
+//     Ok(delete_component.into())
+// }
--- src/models/component_modification/service/delete.rs
+++ src/models/component_modification/service/delete.rs
@@ -1,6 +1,47 @@
 // use crate::database::{db_connection, Pool};
-// use crate::errors::{ServiceError, ServiceResult};
-// use crate::models::user::model::{SlimUser, User};
-// use crate::models::user::util::verify;
+// use crate::errors::{
+//     // ServiceError,
+//     ServiceResult
+// };
+// use crate::models::component_modification::model::{
+//     SlimComponentModification,
+//     ComponentModification
+// };
+// // use crate::models::user::util::verify;
 // use actix_web::web;
 // use diesel::prelude::*;
+// use uuid::Uuid;
+//
+// pub fn delete(
+//     user_uuid: Uuid,
+//     component_modification_delete: Uuid,
+//     pool: web::Data<Pool>
+// ) -> ServiceResult<SlimComponentModification> {
+//     let conn = &db_connection(&pool)?;
+//
+//     delete_component_modification(user_uuid, component_modification_delete, conn)
+// }
+//
+// fn delete_component_modification(
+//     user_uuid: Uuid,
+//     component_modification_delete: Uuid,
+//     conn: &PgConnection
+// ) -> ServiceResult<SlimComponentModification> {
+//     use crate::schema::component_modification_list::dsl::*;
+//
+//     // debug!("fn user_uuid = {}", &user_uuid);
+//     // debug!("fn component_modification_delete = {}", &component_modification_delete);
+//
+//     let delete_component_modification: ComponentModification = component_modification_list
+//         .filter(uuid.eq(user_uuid))
+//         .filter(uuid.eq(component_modification_delete))
+//         .get_result(conn)?;
+//
+//     diesel::delete(
+//         component_modification_list.filter(uuid.eq(delete_component_modification.uuid))
+//     ).execute(conn)?;
+//
+//     // debug!("fn delete_component_modification ={:?}", &delete_component_modification);
+//
+//     Ok(delete_component_modification.into())
+// }
