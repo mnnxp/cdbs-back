3f79e226877e23c3c7eb324805201d8bd524df5f
I_Nosovsky <mananaxep@gmail.com>
2021-07-19 14:38:19 +0300

    delete old and moved models

--- src/models/user_represent/service/mod.rs
+++
@@ -1,6 +0,0 @@
-pub(crate) mod list;
-pub(crate) mod register;
-pub(crate) mod delete;
-
-pub(crate) use register::register;
-pub(crate) use delete::delete;
--- src/models/user_represent/util.rs
+++
@@ -1,5 +0,0 @@
-// use crate::user_represent::model::{LoggedUserRepresent, SlimUserRepresent, UserRepresent};
-// use crate::models::user_represent::model::{UserRepresent};
-// use crate::errors::ServiceError;
-// use argon2rs::argon2i_simple;
-// use uuid::Uuid;
