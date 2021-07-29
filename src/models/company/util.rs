ee64c0342c6cad18f25c5e0c34862d1d08cc0a69
I_Nosovsky <mananaxep@gmail.com>
2021-07-30 01:18:39 +0300

    split check_company_access, add: company_member_role, get_id_type_access

--- src/models/company/util.rs
+++ src/models/company/util.rs
@@ -3,15 +3,7 @@ use crate::errors::ServiceError;
 use diesel::prelude::*;
 use uuid::Uuid;
 
-// pub fn verify_uuid_user(user: &LoggedUser, uuid_user: Uuid) -> Result<bool, ServiceError> {
-//     match user.0 {
-//         None => Err(ServiceError::Unauthorized),
-//         Some(ref user) if user.uuid == uuid_user => Ok(true),
-//         _ => Err(ServiceError::BadRequest("Uuid not correct.".to_string())),
-//         // Some(ref user) => Err(ServiceError::BadRequest(format!("Uuid not correct. UUID1: {}, UUID2: {};", user.uuid, uuid_user))),
-//     }
-// }
-
+/// checking whether the company has a supplier's status
 pub fn check_is_supplier(uuid_company: Uuid, conn: &PgConnection) -> Result<bool, ServiceError> {
     use crate::schema::company_ref::dsl::*;
 
@@ -30,60 +22,77 @@ pub fn check_is_supplier(uuid_company: Uuid, conn: &PgConnection) -> Result<bool
     }
 }
 
+/// checking the availability of the required access level
 pub(crate) fn check_company_access(
     logged_uuid_user: Uuid,
-    input_uuid_company: Uuid,
+    target_uuid_company: Uuid,
     required_access: i32,
     conn: &PgConnection,
 ) -> Result<bool, ServiceError> {
-    // match user.0 {
-        // None => Err(ServiceError::Unauthorized),
-        // Some(ref user) => {
-            use crate::schema::company_member_role::dsl::id_role as member_id_role;
-            use crate::schema::company_member_role::dsl::*;
-            use crate::schema::role_access::dsl::id_role as access_id_role;
-            use crate::schema::role_access::dsl::*;
-            // use crate::schema::company_ref::dsl::*;
+    use crate::schema::company_ref::dsl::*;
 
-            // check user on owner company
-            // let get_id_role_user: i32 = company_member_role
-            //     .filter(uuid_company.eq(uuid_company))
-            //     .filter(uuid_user.eq(uuid_user))
-            //     .select(member_id_role)
-            //     .first(conn)
-            //     .unwrap_or(0);
+    // check user on owner company
+    let user_owner_company = company_ref
+        .filter(uuid.eq(target_uuid_company))
+        .filter(uuid_user.eq(logged_uuid_user))
+        .execute(conn)
+        .unwrap_or(0);
 
-            // find id_role user
-            let get_id_role_user: i32 = company_member_role
-                .filter(uuid_company.eq(input_uuid_company))
-                .filter(uuid_user.eq(logged_uuid_user))
-                .select(member_id_role)
-                .first(conn)
-                .unwrap_or(0);
+    if user_owner_company == 1 {
+        return Ok(true)
+    }
 
-            // find level access for id_role
-            let get_id_type_access: i32 = role_access
-                .filter(access_id_role.eq(get_id_role_user))
-                .select(id_type_access)
-                .first(conn)
-                .unwrap_or(0);
+    let found_id_type_access: i32 = get_id_type_access(
+        company_member_role(
+            logged_uuid_user,
+            target_uuid_company,
+            conn,
+        ),
+        conn
+    );
 
-            match get_id_type_access {
-                1..=i32::MAX => {
-                    // debug!("get_id_type_access ({:?}) < required_access ({:?})", &get_id_type_access, &required_access);
-                    // delete represent and save delete data for send response
-                    if get_id_type_access < required_access {
-                        Ok(true)
-                    } else {
-                        Err(ServiceError::BadRequest(
-                            "You have insufficient access level.".to_string(),
-                        ))
-                    }
-                }
-                _ => Err(ServiceError::BadRequest("You not have access.".to_string())),
+    match found_id_type_access {
+        1..=i32::MAX => {
+            // debug!("get_id_type_access ({:?}) < required_access ({:?})", &get_id_type_access, &required_access);
+            // delete represent and save delete data for send response
+            if found_id_type_access < required_access {
+                Ok(true)
+            } else {
+                Err(ServiceError::BadRequest(
+                    "You have insufficient access level.".to_string(),
+                ))
             }
-        // }
-        // _ => Err(ServiceError::BadRequest("Uuid not correct.".to_string())),
-        // Some(ref user) => Err(ServiceError::BadRequest(format!("Uuid not correct. UUID1: {}, UUID2: {};", user.uuid, uuid_user))),
-    // }
+        }
+        _ => Err(ServiceError::BadRequest("You not have access.".to_string())),
+    }
+}
+
+pub(crate) fn company_member_role(
+    logged_uuid_user: Uuid,
+    target_uuid_company: Uuid,
+    conn: &PgConnection,
+) -> i32 {
+    use crate::schema::company_member_role::dsl::*;
+
+    // find id_role user
+    company_member_role
+        .filter(uuid_company.eq(target_uuid_company))
+        .filter(uuid_user.eq(logged_uuid_user))
+        .select(id_role)
+        .first(conn)
+        .unwrap_or(0)
+}
+
+pub(crate) fn get_id_type_access(
+    id_role_user: i32,
+    conn: &PgConnection,
+) -> i32 {
+    use crate::schema::role_access::dsl::*;
+
+    // find level access for id_role
+    role_access
+        .filter(id_role.eq(id_role_user))
+        .select(id_type_access)
+        .first(conn)
+        .unwrap_or(0)
 }
