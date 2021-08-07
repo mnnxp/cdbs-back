0b878cbd14f9628b36eb5b387e5c100c22deb8da
I_Nosovsky <mananaxep@gmail.com>
2021-08-07 22:49:50 +0300

    change name of *** to target_***

--- src/models/company/util.rs
+++ src/models/company/util.rs
@@ -1,14 +1,17 @@
 use crate::errors::ServiceError;
-// use crate::models::user::model::LoggedUser;
+// use crate::models::company::model::Company;
 use diesel::prelude::*;
 use uuid::Uuid;
 
 /// checking whether the company has a supplier's status
-pub fn check_is_supplier(uuid_company: Uuid, conn: &PgConnection) -> Result<bool, ServiceError> {
+pub fn check_is_supplier(
+    target_uuid_company: Uuid,
+    conn: &PgConnection
+) -> Result<bool, ServiceError> {
     use crate::schema::company_ref::dsl::*;
 
     let get_company_status: bool = company_ref
-        .filter(uuid.eq(uuid_company))
+        .filter(uuid.eq(target_uuid_company))
         .select(is_supplier)
         .first(conn)
         .unwrap_or(false);
@@ -24,7 +27,7 @@ pub fn check_is_supplier(uuid_company: Uuid, conn: &PgConnection) -> Result<bool
 
 /// checking the availability of the required access level
 pub(crate) fn check_company_access(
-    logged_uuid_user: Uuid,
+    target_uuid_user: Uuid,
     target_uuid_company: Uuid,
     required_access: i32,
     conn: &PgConnection,
@@ -34,7 +37,7 @@ pub(crate) fn check_company_access(
     // check user on owner company
     let user_owner_company = company_ref
         .filter(uuid.eq(target_uuid_company))
-        .filter(uuid_user.eq(logged_uuid_user))
+        .filter(uuid_user.eq(target_uuid_user))
         .execute(conn)
         .unwrap_or(0);
 
@@ -44,7 +47,7 @@ pub(crate) fn check_company_access(
 
     let found_id_type_access: i32 = get_id_type_access(
         company_member_role(
-            logged_uuid_user,
+            target_uuid_user,
             target_uuid_company,
             conn,
         ),
@@ -54,7 +57,6 @@ pub(crate) fn check_company_access(
     match found_id_type_access {
         1..=i32::MAX => {
             // debug!("get_id_type_access ({:?}) < required_access ({:?})", &get_id_type_access, &required_access);
-            // delete represent and save delete data for send response
             if found_id_type_access < required_access {
                 Ok(true)
             } else {
@@ -68,7 +70,7 @@ pub(crate) fn check_company_access(
 }
 
 pub(crate) fn company_member_role(
-    logged_uuid_user: Uuid,
+    target_uuid_user: Uuid,
     target_uuid_company: Uuid,
     conn: &PgConnection,
 ) -> i32 {
@@ -77,22 +79,55 @@ pub(crate) fn company_member_role(
     // find id_role user
     company_member_role
         .filter(uuid_company.eq(target_uuid_company))
-        .filter(uuid_user.eq(logged_uuid_user))
+        .filter(uuid_user.eq(target_uuid_user))
         .select(id_role)
         .first(conn)
         .unwrap_or(0)
 }
 
+/// Search id_type_access for target id_role
 pub(crate) fn get_id_type_access(
-    id_role_user: i32,
+    target_id_role: i32,
     conn: &PgConnection,
 ) -> i32 {
     use crate::schema::role_access::dsl::*;
 
     // find level access for id_role
     role_access
-        .filter(id_role.eq(id_role_user))
+        .filter(id_role.eq(target_id_role))
         .select(id_type_access)
         .first(conn)
         .unwrap_or(0)
+}
+
+/// Search for owned companies
+pub(crate) fn get_companies_owned_by_user(
+    target_uuid_user: Uuid,
+    conn: &PgConnection,
+) -> Vec<Uuid> {
+    use crate::schema::company_ref::dsl::*;
+
+    company_ref
+        .filter(uuid_user.eq(target_uuid_user))
+        .select(uuid)
+        .load(conn)
+        .unwrap_or_default()
+}
+
+/// Search for companies the user belongs to
+pub(crate) fn _get_companies_with_member_by_user(
+    target_uuid_user: Uuid,
+    conn: &PgConnection,
+) -> Vec<(Uuid, i32)> {
+    use crate::schema::company_member_role::dsl::*;
+
+    // find id_role user
+    company_member_role
+        .filter(uuid_user.eq(target_uuid_user))
+        .select((
+            uuid_company,
+            id_role
+        ))
+        .load(conn)
+        .unwrap_or_default()
 }
