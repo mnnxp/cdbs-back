34f0e8a15147525c3121726f2cf151e70615d6af
I_Nosovsky <mananaxep@gmail.com>
2021-08-17 15:14:35 +0300

    add models for spec

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,6 +2,7 @@ pub(crate) mod company;
 pub(crate) mod component;
 pub(crate) mod file;
 pub(crate) mod language;
+pub(crate) mod spec;
 pub(crate) mod standard;
 pub(crate) mod user;
 // pub(crate) mod user_represent;
---
+++ src/models/spec/service/list.rs
@@ -0,0 +1,61 @@
+use crate::database::{get_conn, PooledConnection};
+use crate::errors::{
+    // ServiceError,
+    ServiceResult
+};
+// use crate::graphql::model::Context;
+use async_graphql::Context;
+use crate::models::spec::model::SpecTranslateList;
+use diesel::prelude::*;
+
+
+pub(crate) fn get_specs(
+    context: &Context<'_>,
+    target_id_spec: Vec<i32>,
+    limit: i32,
+    offset: i32,
+) -> ServiceResult<Vec<SpecTranslateList>> {
+    match target_id_spec {
+        target_id_spec if target_id_spec.is_empty() => find_all_specs(context, limit, offset),
+        target_id_spec => find_id_specs(context, target_id_spec, limit, offset)
+        // _ => ServiceResult::Err(ServiceError::BadRequest("What?".to_string()))
+    }
+}
+
+fn find_all_specs(
+    context: &Context<'_>,
+    limit: i32,
+    offset: i32,
+) -> ServiceResult<Vec<SpecTranslateList>> {
+    use crate::schema::spec_translate_list::dsl::*;
+    let conn: &PooledConnection = &get_conn(context)?;
+    // let target_id_lang: IdLanguage = context.into();
+
+    let set_id_lang = crate::models::user::get_set_language(context);
+
+    Ok(spec_translate_list
+        .filter(id_lang.eq(set_id_lang))
+        .limit(limit as i64)
+        .offset(offset as i64)
+        .load::<SpecTranslateList>(conn)?)
+}
+
+fn find_id_specs(
+    context: &Context<'_>,
+    target_id_spec: Vec<i32>,
+    limit: i32,
+    offset: i32,
+) -> ServiceResult<Vec<SpecTranslateList>> {
+    use crate::schema::spec_translate_list::dsl::*;
+    let conn: &PooledConnection = &get_conn(context)?;
+
+    let set_id_lang = crate::models::user::get_set_language(context);
+
+    Ok(spec_translate_list
+        // .filter(id_lang.eq_any(target_id_lang))
+        .filter(id_spec.eq_any(target_id_spec))
+        .filter(id_lang.eq(set_id_lang))
+        .limit(limit as i64)
+        .offset(offset as i64)
+        .load::<SpecTranslateList>(conn)?)
+}
