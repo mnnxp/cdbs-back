1cbe0ca53a50663a56290378352ea0b98f31b26a
I_Nosovsky <mananaxep@gmail.com>
2021-08-24 12:44:51 +0300

    delete duplicate code for component_type

--- src/models/component/relate/component_type/service/add.rs
+++ src/models/component/relate/component_type/service/add.rs
@@ -1,28 +1,45 @@
 use crate::errors::{ ServiceError, ServiceResult };
 use crate::models::component::component_type::model::{
-    InsertableComponentType, ComponentType, IptComponentTypeData
+    ComponentType,
+    ComponentTypeTranslateList,
+    IptComponentTypeTranslateListData,
+    InsertableComponentTypeTranslateList,
 };
 use diesel::prelude::*;
 // use uuid::Uuid;
 
 pub(crate) fn create_component_type(
-    new_component_type_data: IptComponentTypeData,
+    new_component_type_data: IptComponentTypeTranslateListData,
     conn: &PgConnection
-) -> ServiceResult<ComponentType> {
-    use crate::schema::component_type_ref::dsl::*;
+) -> ServiceResult<ComponentTypeTranslateList> {
+    use crate::schema::component_type_translate_list::dsl as component_type_translate_list;
 
-    let flag_found_component_type = component_type_ref
-        .filter(id_lang.eq(&new_component_type_data.id_lang))
-        .filter(name.eq(&new_component_type_data.name))
-        .select(id)
+    let flag_found_component_type = component_type_translate_list::component_type_translate_list
+        .filter(component_type_translate_list::id_lang.eq(&new_component_type_data.id_lang))
+        .filter(component_type_translate_list::component_type.eq(&new_component_type_data.component_type))
+        .select(component_type_translate_list::id_component_type)
         .first::<i32>(conn).unwrap_or(0);
 
     // debug!("fn create_component_type START SEARCH ={:?}", flag_found_component_type);
 
     match flag_found_component_type {
         0 => {
-            let new_component_type_data: InsertableComponentType = new_component_type_data.into();
-            let inserted_component_type_data: ComponentType = diesel::insert_into(component_type_ref)
+            let new_id_component_type = {
+                use crate::schema::component_type_ref::dsl as component_type_ref;
+
+                let new_component_type: ComponentType = diesel::insert_into(component_type_ref::component_type_ref)
+                    .default_values()
+                    .get_result(conn)?;
+
+                new_component_type.id
+            };
+
+            let new_component_type_data = InsertableComponentTypeTranslateList {
+                id_component_type: new_id_component_type,
+                id_lang: new_component_type_data.id_lang,
+                component_type: new_component_type_data.component_type,
+            };
+            let inserted_component_type_data: ComponentTypeTranslateList = diesel::insert_into(component_type_translate_list::component_type_translate_list)
                 .values(&new_component_type_data)
                 .get_result(conn)?;
             Ok(inserted_component_type_data)
