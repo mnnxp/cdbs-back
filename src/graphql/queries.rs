49f879e0a8fe49a5eafc0ffe5544d41f7eae9f59
I_Nosovsky <mananaxep@gmail.com>
2021-08-24 14:15:12 +0300

    delete duplicate code for component modification

--- src/graphql/queries.rs
+++ src/graphql/queries.rs
@@ -5,8 +5,8 @@ use crate::models::company::company_represent::model::ShowCompanyRepresent;
 use crate::models::company::company_represent::service as company_represent;
 use crate::models::company::model::ShowCompany;
 use crate::models::company::service as company;
-use crate::models::component::component_modification::model::ComponentModification;
-use crate::models::component::component_modification::service as component_modification;
+// use crate::models::component::component_modification::model::ComponentModification;
+// use crate::models::component::component_modification::service as component_modification;
 // use crate::models::component::license as component_license;
 // use crate::models::component::license::model::LicenseComponent;
 use crate::models::component::model::Component;
@@ -249,31 +249,31 @@ impl QueryRoot {
         component::list::find_uuid_component(context, Uuid::parse_str(&uuid_component)?)
     }
 
-    async fn component_modification(
-        &self,
-        context: &Context<'_>,
-        uuid_component: Option<String>,
-        limit: Option<i32>,
-        offset: Option<i32>,
-    ) -> ServiceResult<Vec<ComponentModification>> {
-        // authorization check
-        crate::models::user::util::check_authorized(context)?;
-
-        let uuid_component = match uuid_component {
-            None => Uuid::nil(),
-            Some(uuid_component) => Uuid::parse_str(&uuid_component)?,
-        };
-
-        let limit: i32 = limit.unwrap_or(100);
-        let offset: i32 = offset.unwrap_or(0);
-
-        component_modification::list::get_component_modifications(
-            context,
-            uuid_component,
-            limit,
-            offset,
-        )
-    }
+    // async fn component_modification(
+    //     &self,
+    //     context: &Context<'_>,
+    //     uuid_component: Option<String>,
+    //     limit: Option<i32>,
+    //     offset: Option<i32>,
+    // ) -> ServiceResult<Vec<ComponentModification>> {
+    //     // authorization check
+    //     crate::models::user::util::check_authorized(context)?;
+    //
+    //     let uuid_component = match uuid_component {
+    //         None => Uuid::nil(),
+    //         Some(uuid_component) => Uuid::parse_str(&uuid_component)?,
+    //     };
+    //
+    //     let limit: i32 = limit.unwrap_or(100);
+    //     let offset: i32 = offset.unwrap_or(0);
+    //
+    //     component_modification::list::get_component_modifications(
+    //         context,
+    //         uuid_component,
+    //         limit,
+    //         offset,
+    //     )
+    // }
 
     async fn licenses(
         &self,
--- src/models/component/component_modification/service/mod.rs
+++ src/models/component/component_modification/service/mod.rs
@@ -1,6 +1,2 @@
-pub(crate) mod list;
 pub(crate) mod register;
-pub(crate) mod delete;
-
-// pub(crate) use register::register;
-// pub(crate) use delete::delete;
+// pub(crate) mod delete;
