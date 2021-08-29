592e2eecdea55e88d32e1f8b552a3be2583777e7
I_Nosovsky <mananaxep@gmail.com>
2021-08-29 19:03:04 +0300

    add args for access to backblaze

--- src/cli_args.rs
+++ src/cli_args.rs
@@ -1,6 +1,6 @@
 use structopt::StructOpt;
 
-/// Juniper (GraphQl API), Diesel PostgreSQL, session authentication and JWT boilerplate server
+/// GraphQl API, Diesel PostgreSQL, session authentication and JWT boilerplate server
 #[derive(StructOpt, Debug, Clone)]
 #[structopt(name = "cdbs-back")]
 pub struct Opt {
@@ -33,4 +33,20 @@ pub struct Opt {
     /// this is used for cookie and JWT
     #[structopt(long, env = "AUTH_DURATION_IN_HOUR", default_value = "24")]
     pub auth_duration_in_hour: u16,
+
+    /// Backblaze
+    #[structopt(long, env = "B2_APPLICATION_KEY_ID", default_value = "002cb0e1d5d32050000000002")]
+    pub b2_application_key_id: String,
+    #[structopt(long, env = "B2_APPLICATION_KEY", default_value = "K002F/8X1d6VI1ZyYv5KyFKbEv+V2LY")]
+    pub b2_application_key: String,
+    #[structopt(long, env = "B2_AUTHORIZATION_TOKEN", default_value = "4_002cb0e1d5d32050000000002_019e8cef_cc60fb_acct_UlB8zXtzZNYNoA6BfNFlKQShbbU=")]
+    pub b2_authorization_token: String,
+    #[structopt(long, env = "B2_ACCOUNT_ID", default_value = "cb0e1d5d3205")]
+    pub b2_account_id: String,
+    #[structopt(long, env = "B2_BUCKET_ID", default_value = "1c8ba08e816d056d73b20015")]
+    pub b2_bucket_id: String,
+    #[structopt(long, env = "B2_API_URL", default_value = "https://api002.backblazeb2.com")]
+    pub b2_api_url: String,
+    #[structopt(long, env = "B2_CAPABILITIES", default_value = "listFiles, readFiles, shareFiles, writeFiles, deleteFiles")]
+    pub b2_capabilities: String,
 }
