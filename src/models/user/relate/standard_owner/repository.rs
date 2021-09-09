a8a88121315a5be29835ab4ebce06a94757d1daf
I_Nosovsky <mananaxep@gmail.com>
2021-09-09 16:01:09 +0300

    add impl for get the user's standards

---
+++ src/models/user/relate/standard_owner/repository.rs
@@ -0,0 +1,37 @@
+use crate::errors::ServiceResult;
+use crate::models::standard::model::ShowStandardShort;
+use crate::schema::standard_ref::dsl as standard_ref;
+use diesel::prelude::*;
+use uuid::Uuid;
+
+impl ShowStandardShort {
+    /// get list subscribers for standard
+    pub fn get_by_user_uuid(
+        target_user_uuid: &Uuid,
+        set_id_lang: &i32,
+        conn: &PgConnection,
+    ) -> ServiceResult<Vec<ShowStandardShort>> {
+        let target_standards_uuids = standard_ref::standard_ref
+            .filter(standard_ref::uuid_user.eq(target_user_uuid))
+            .select(standard_ref::uuid)
+            .load::<Uuid>(conn)
+            .expect("Fail load uuid list target user");
+
+        ShowStandardShort::get_list_by_uuids(
+            &target_standards_uuids,
+            target_user_uuid,
+            set_id_lang,
+            conn,
+        )
+    }
+
+    /// Count subscribers for standard
+    pub fn get_count_by_user_uuid(
+        target_user_uuid: &Uuid,
+        conn: &PgConnection,
+    ) -> ServiceResult<i32> {
+        Ok(standard_ref::standard_ref
+            .filter(standard_ref::uuid_user.eq(target_user_uuid))
+            .execute(conn)? as i32)
+    }
+}
