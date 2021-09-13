97b90c3284e4e80a3876889e7ed4b29b98743cc4
I_Nosovsky <mananaxep@gmail.com>
2021-09-13 17:17:57 +0300

    remove extra code

--- src/models/component/component_modification/relate/file_to_set_modification/service/list.rs
+++ src/models/component/component_modification/relate/file_to_set_modification/service/list.rs
@@ -1,33 +1,10 @@
 use crate::database::{get_conn, PooledConnection};
-use crate::errors::{
-    ServiceError,
-    ServiceResult
-};
-// use crate::graphql::model::Context;
-use async_graphql::Context;
+use crate::errors::ServiceResult;
 use crate::models::component::component_modification::file_to_set_modification::model::FileToSetModification;
+use async_graphql::Context;
 use diesel::prelude::*;
 
-
 pub(crate) fn get_files_set_modification(
-    cxt: &Context<'_>,
-    target_set_id: i32,
-    limit: i32,
-    offset: i32,
-) -> ServiceResult<Vec<FileToSetModification>> {
-    let mut variant_selection: u8 = 0;
-    if target_set_id > 0 {
-        variant_selection += 1;
-    }
-
-    match variant_selection {
-        // 0 => find_all_set_files(cxt, limit, offset),
-        1 => find_files_for_set_modification(cxt, target_set_id, limit, offset),
-        _ => ServiceResult::Err(ServiceError::BadRequest("What?".to_string()))
-    }
-}
-
-fn find_files_for_set_modification(
     cxt: &Context<'_>,
     target_set_id: i32,
     limit: i32,
