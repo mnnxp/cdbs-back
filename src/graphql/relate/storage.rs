6c15f09c72fe64d2a2dee63e0da7f0d9859e0718
I_Nosovsky <mananaxep@gmail.com>
2021-09-18 18:54:03 +0300

    update API for upload completed and removing file

--- src/graphql/relate/storage.rs
+++ src/graphql/relate/storage.rs
@@ -3,9 +3,7 @@ use uuid::Uuid;
 
 use crate::database::{get_pool, get_conn, PooledConnection};
 use crate::errors::ServiceResult;
-use crate::models::user::model::TargetUser;
 use crate::models::relate_ref::file;
-use crate::storage::wrapper::storage_access::get_user_storage_access;
 
 #[derive(Default)]
 pub struct StorageQuery;
@@ -37,33 +35,51 @@ impl StorageQuery {
 #[Object]
 impl StorageMutation {
     /// Сonfirmation of successful upload of files to storage
-    async fn upload_completed(&self, cxt: &Context<'_>, file_id: String) -> ServiceResult<i32> {
+    async fn upload_completed(
+        &self,
+        cxt: &Context<'_>,
+        files_uuids: Vec<String>,
+    ) -> ServiceResult<i32> {
         let pool = get_pool(cxt)?;
 
-        let target_user = TargetUser::from(&crate::models::user::get_logged_user_uuid(cxt, true)?);
+        let logged_user_uuid = crate::models::user::get_logged_user_uuid(cxt, true)?;
 
-        Ok(file::service::update::confirm_upload(&target_user, &file_id, pool).await?)
+        let mut target_files_uuids = Vec::new();
+        for x in files_uuids.iter() {
+            target_files_uuids.push(Uuid::parse_str(x).unwrap());
+        }
+
+        if target_files_uuids.is_empty() {
+            return Ok(0) // <-- Not found uuids, just return 0
+        }
+
+        let res = file::service::update::confirm_upload(
+            &logged_user_uuid,
+            &target_files_uuids,
+            pool
+        ).await?;
+
+        Ok(res)
     }
 
-    async fn update_storage_access(
-        &self, cxt: &Context<'_>,
-    ) -> ServiceResult<bool> {
+    /// Delete file in storage
+    async fn delete_file(
+        &self,
+        cxt: &Context<'_>,
+        file_uuid: String,
+    ) -> ServiceResult<i32> {
         let pool = get_pool(cxt)?;
-        // let conn = pool.get().unwrap();
 
-        // storage access data
-        match get_user_storage_access(
-            TargetUser::from(&crate::models::user::get_logged_user_uuid(cxt, true)?),
+        let logged_user_uuid = crate::models::user::get_logged_user_uuid(cxt, true)?;
+
+        let target_file_uuid = Uuid::parse_str(&file_uuid).unwrap();
+
+        let res = file::service::delete::delete_file_by_uuid(
+            &logged_user_uuid,
+            &target_file_uuid,
             pool
-        ).await {
-            Ok(data) => {
-                debug!("New storage access data: {:?}", data);
-                Ok(true)
-            },
-            Err(err) => {
-                debug!("Fail get new storage access data: {:?}", err);
-                Ok(false)
-            }
-        }
+        ).await?;
+
+        Ok(res)
     }
 }
