7bdae36d03db0f90955b6583dbed52cf59119120
I_Nosovsky <mananaxep@gmail.com>
2021-09-18 18:35:15 +0300

    remove params for b2

--- src/cli_args.rs
+++ src/cli_args.rs
@@ -1,5 +1,4 @@
 use structopt::StructOpt;
-use uuid::Uuid;
 
 /// GraphQl API, Diesel PostgreSQL, session authentication and JWT boilerplate server
 #[derive(StructOpt, Debug, Clone)]
@@ -34,20 +33,4 @@ pub struct Opt {
     /// this is used for cookie and JWT
     #[structopt(long, env = "AUTH_DURATION_IN_HOUR", default_value = "24")]
     pub(crate) auth_duration_in_hour: u16,
-
-    /// Backblaze
-    #[structopt(long, env = "UUID_MAIN_ACCESS_B2", parse(try_from_str), default_value = "31ecc6f80c094a59a2d534b5b833e59b")]
-    pub(crate) uuid_main_access_b2: Uuid, // <-- for storing the master key and token
-    #[structopt(long, env = "B2_ACCOUNT_ID", default_value = "cb0e1d5d3205")]
-    pub(crate) b2_account_id: String,
-    #[structopt(long, env = "B2_CAPABILITIES", default_value = "listFiles,readFiles,shareFiles,writeFiles,deleteFiles")]
-    pub(crate) b2_capabilities: String,
-    #[structopt(long, env = "B2_BUCKET_ID", default_value = "1c8ba08e816d056d73b20015")]
-    pub(crate) b2_bucket_id: String,
-    #[structopt(long, env = "B2_BUCKET", default_value = "cdbs-test")]
-    pub(crate) b2_bucket: String,
-    #[structopt(long, env = "S3_REGION", default_value = "s3.us-west-002")]
-    pub(crate) s3_region: String,
-    #[structopt(long, env = "S3_ENDPOINT", default_value = "backblazeb2.com")]
-    pub(crate) s3_endpoint: String,
 }
