631a63284cc44aae8925eeee3274edcb172dedff
I_Nosovsky <mananaxep@gmail.com>
2021-09-18 18:20:09 +0300

    remove integration with b2

--- src/storage/backblaze/b2/mod.rs
+++
@@ -1,4 +0,0 @@
-pub(crate) mod b2_authorize_account;
-pub(crate) mod b2_create_key;
-pub(crate) mod b2_download_file_by_id;
-pub(crate) mod b2_get_upload_url;
--- src/storage/backblaze/mod.rs
+++
@@ -1,4 +0,0 @@
-pub(crate) mod b2;
-pub(crate) mod b2_types;
-
-pub(crate) use b2::*;
