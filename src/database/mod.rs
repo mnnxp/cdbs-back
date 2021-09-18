d74ea0bda3b3128b347e271ce97412a200df1205
I_Nosovsky <mananaxep@gmail.com>
2021-09-18 18:46:51 +0300

    fix mistake name for pool

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -7,7 +7,7 @@ use diesel::r2d2::PoolError;
 type ConnectionManager = diesel::r2d2::ConnectionManager<diesel::PgConnection>;
 pub(crate) type Pool = diesel::r2d2::Pool<ConnectionManager>;
 pub(crate) type PooledConnection = diesel::r2d2::PooledConnection<ConnectionManager>;
-pub(crate) type PgConn = diesel::r2d2::Pool<diesel::r2d2::ConnectionManager<diesel::PgConnection>>;
+pub(crate) type PgPool = diesel::r2d2::Pool<diesel::r2d2::ConnectionManager<diesel::PgConnection>>;
 
 pub(crate) fn db_connection(pool: &Pool) -> Result<PooledConnection, ServiceError> {
     let conn = pool.get().map_err(|_| ServiceError::UnableToConnectToDb)?;
@@ -21,7 +21,7 @@ pub(crate) fn get_conn(cxt: &Context) -> Result<PooledConnection, ServiceError> 
     Ok(conn)
 }
 
-pub(crate) fn get_pool(cxt: &Context) -> Result<PgConn, ServiceError> {
+pub(crate) fn get_pool(cxt: &Context) -> Result<PgPool, ServiceError> {
     let pool = cxt
         .data::<Pool>().expect("Can't get pool")
         .clone();
