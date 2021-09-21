0a0b63a75d35cd3d42785de09edbacf8a7a5d5ce
I_Nosovsky <mananaxep@gmail.com>
2021-09-21 18:02:55 +0300

    code optimization and add error messages

--- src/storage/presigned_url.rs
+++ src/storage/presigned_url.rs
@@ -1,4 +1,4 @@
-use crate::errors::ServiceResult;
+use crate::errors::{ServiceResult, ServiceError};
 use crate::storage::model::StorageAccess;
 use crate::storage::s3::Aws;
 
@@ -7,18 +7,20 @@ pub(crate) fn download_presigned_url(
     access_storage: &StorageAccess,
     path_file: &str,
 ) -> ServiceResult<String> {
-    let aws_access = Aws::from(access_storage);
-
-    let presigned_url = aws_access.put_download_signed_url(
+    match Aws::from(access_storage).put_download_signed_url(
         &access_storage.bucket,
         path_file,
         800_u64, // <-- todo!(temp solution)
-    )?;
-
-    debug!("Presigned url: {:#?}", presigned_url);
-
-    // Ok(String::from("Temp string for presigned url for download"))
-    Ok(presigned_url)
+    ) {
+        Ok(presigned_url) => {
+            debug!("Presigned url for download: {:#?}", presigned_url);
+            Ok(presigned_url)
+        },
+        Err(err) => {
+            debug!("Failed make presign-url: {:#?}", err);
+            Err(ServiceError::BadRequest("Failed make presign-url".to_string()))
+        }
+    }
 }
 
 /// Gets presigned url for upload files to storage
@@ -26,16 +28,18 @@ pub(crate) fn upload_presigned_url(
     access_storage: &StorageAccess,
     path_file: &str,
 ) -> ServiceResult<String> {
-    let aws_access = Aws::from(access_storage);
-
-    let presigned_url = aws_access.get_upload_signed_url(
+    match Aws::from(access_storage).get_upload_signed_url(
         &access_storage.bucket,
         path_file,
         800_u64, // <-- todo!(temp solution)
-    )?;
-
-    debug!("Presigned url for upload: {:#?}", presigned_url);
-
-    // Ok(String::from("Temp string for presigned url for upload"))
-    Ok(presigned_url)
+    ) {
+        Ok(presigned_url) => {
+            debug!("Presigned url for upload: {:#?}", presigned_url);
+            Ok(presigned_url)
+        },
+        Err(err) => {
+            debug!("Failed make presign-url: {:#?}", err);
+            Err(ServiceError::BadRequest("Failed make presign-url".to_string()))
+        }
+    }
 }
