f4220e4ff4cb9a8c8580bfce2dee4e7e08efc0e9
I_Nosovsky <mananaxep@gmail.com>
2021-10-02 00:31:29 +0300

    make util for check access

---
+++ src/models/company/access/util.rs
@@ -0,0 +1,235 @@
+use crate::errors::{ServiceResult, ServiceError};
+use crate::models::company::member::role::model::RoleMember;
+use diesel::prelude::*;
+use uuid::Uuid;
+
+/// Checking company owner, return bool
+pub fn check_is_owner(
+    target_user_uuid: &Uuid,
+    target_company_uuid: &Uuid,
+    conn: &PgConnection
+) -> bool {
+    use crate::schema::company_ref::dsl::*;
+
+    let check_owner_company = company_ref
+        .filter(user_uuid.eq(target_user_uuid)
+        .and(uuid.eq(target_company_uuid)))
+        .limit(1)
+        .execute(conn);
+
+    match check_owner_company {
+        Ok(count) if count == 1 => true,
+        Ok(_) => false,
+        Err(err) => {
+            debug!("Failed check data: {:?}", err);
+            false
+        },
+    }
+}
+
+/// Checking companies for user owner,
+/// return true if user owner any of companies
+pub fn check_is_owner_any(
+    target_user_uuid: &Uuid,
+    target_companies_uuids: &[Uuid],
+    conn: &PgConnection
+) -> bool {
+    use crate::schema::company_ref::dsl::*;
+
+    let check_owner_companies = company_ref
+        .filter(user_uuid.eq(target_user_uuid)
+        .and(uuid.eq_any(target_companies_uuids)))
+        .limit(1)
+        .execute(conn);
+
+    match check_owner_companies {
+        Ok(count) if count == 1 => true,
+        Ok(_) => false,
+        Err(err) => {
+            debug!("Failed check data: {:?}", err);
+            false
+        },
+    }
+}
+
+/// Checking onwed company
+/// Return error if user not owned
+pub fn check_is_owner_with_err(
+    target_user_uuid: &Uuid,
+    target_company_uuid: &Uuid,
+    conn: &PgConnection
+) -> ServiceResult<bool> {
+    match check_is_owner(target_user_uuid, target_company_uuid, conn) {
+        true => Ok(true),
+        false => Err(ServiceError::BadRequest(
+            "Access denied".to_string(),
+        )),
+    }
+}
+
+/// Gets list of users uuids that have need level access to a company
+pub(crate) fn _get_users_have_access_to_company(
+    target_company_uuid: &Uuid,
+    need_access_level: &i32,
+    conn: &PgConnection
+) -> ServiceResult<Vec<Uuid>> {
+    use crate::schema::company_member_list::dsl::*;
+
+    let suitable_role = RoleMember::get_roles_for_type_access(
+        need_access_level,
+        conn
+    );
+
+    if suitable_role.is_empty() {
+        return Err(ServiceError::BadRequest(
+            "Not found set access for target role".to_string()
+        ))
+    }
+
+    let users_uuids = company_member_list
+        .filter(company_uuid.eq(target_company_uuid)
+        .and(role_id.eq_any(&suitable_role)))
+        .select(user_uuid)
+        .load::<Uuid>(conn);
+
+    match users_uuids {
+        Ok(ur_uuids) if !ur_uuids.is_empty() => Ok(ur_uuids),
+        // not found companies with need access
+        Ok(_) => Err(ServiceError::BadRequest(
+            "Mot found users with access target company".to_string()
+        )),
+        Err(err) => {
+            debug!("Failed check data: {:?}", err);
+            Err(ServiceError::BadRequest(
+                "Failed check data".to_string()
+            ))
+        },
+    }
+}
+
+/// Checking the availability of the required access level
+/// with ownership check
+pub(crate) fn check_company_access(
+    target_user_uuid: &Uuid,
+    target_company_uuid: &Uuid,
+    required_access: &i32,
+    conn: &PgConnection,
+) -> ServiceResult<bool> {
+    // check user on owner company
+    if check_is_owner(
+        target_user_uuid,
+        target_company_uuid,
+        conn
+    ) {
+        return Ok(true)
+    }
+
+    let found_type_access_id: i32 = get_type_access_id(
+        &company_member_list(
+            target_user_uuid,
+            target_company_uuid,
+            conn,
+        ),
+        conn
+    );
+
+    match found_type_access_id {
+        1..=i32::MAX => {
+            // debug!("get_type_access_id ({:?}) < required_access ({:?})", &get_type_access_id, &required_access);
+            if &found_type_access_id < required_access {
+                Ok(true)
+            } else {
+                Err(ServiceError::BadRequest(
+                    "Access denied".to_string(),
+                ))
+            }
+        }
+        _ => Err(ServiceError::BadRequest("Access denied".to_string())),
+    }
+}
+
+pub(crate) fn company_member_list(
+    target_user_uuid: &Uuid,
+    target_company_uuid: &Uuid,
+    conn: &PgConnection,
+) -> i32 {
+    use crate::schema::company_member_list::dsl::*;
+
+    // find role_id user
+    company_member_list
+        .filter(company_uuid.eq(target_company_uuid))
+        .filter(user_uuid.eq(target_user_uuid))
+        .select(role_id)
+        .first(conn)
+        .unwrap_or(0)
+}
+
+/// Search type_access_id for target role_id
+pub(crate) fn get_type_access_id(
+    target_role_id: &i32,
+    conn: &PgConnection,
+) -> i32 {
+    use crate::schema::role_access::dsl::*;
+
+    // find level access for role_id
+    role_access
+        .filter(role_id.eq(target_role_id))
+        .select(type_access_id)
+        .first(conn)
+        .unwrap_or(0)
+}
+
+/// Get role IDs for desired level access
+pub(crate) fn get_roles_ids_for_access(
+    need_level_access: &i32,
+    conn: &PgConnection
+) -> ServiceResult<Vec<i32>> {
+    use crate::schema::role_access::dsl::*;
+
+    let roles_ids = role_access
+        .filter(type_access_id.le(need_level_access)) // <-- filter access < or = need_level_access
+        .select(role_id)
+        .load(conn);
+
+    match roles_ids {
+        Ok(rs_ids) => Ok(rs_ids),
+        Err(err) => {
+            debug!("Failed get data: {:?}", err);
+            Err(ServiceError::BadRequest(
+                "Failed get data".to_string()
+            ))
+        },
+    }
+}
+
+/// Check have user among companies employees with a suitable role
+pub(crate) fn check_clerk_with_suitable_role(
+    target_user_uuid: &Uuid,
+    target_companies_uuids: &[Uuid],
+    need_roles_ids: &[i32],
+    conn: &PgConnection
+) -> bool {
+    use crate::schema::company_member_list::dsl::*;
+
+    // if user owner any of companies
+    if check_is_owner_any(target_user_uuid, target_companies_uuids, conn) {
+        return true
+    }
+
+    let find_provided_role = company_member_list
+        .filter(user_uuid.eq(target_user_uuid)
+        .and(company_uuid.eq_any(target_companies_uuids)
+        .and(role_id.eq_any(need_roles_ids))))
+        .limit(1)
+        .execute(conn);
+
+    match find_provided_role {
+        Ok(provided_role) if provided_role == 1 => true,
+        Ok(_) => false,
+        Err(err) => {
+            debug!("Failed get data: {:?}", err);
+            false
+        },
+    }
+
+}
