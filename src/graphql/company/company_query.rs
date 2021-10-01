33c9808d444b4ede336494286d63a06e4dc6ee64
I_Nosovsky <mananaxep@gmail.com>
2021-10-02 00:39:44 +0300

    make api for get roles and members

--- src/graphql/company/company_query.rs
+++ src/graphql/company/company_query.rs
@@ -1,10 +1,12 @@
 use crate::errors::ServiceResult;
 use crate::database::{get_conn, PooledConnection};
+use crate::models::user;
+use crate::models::company::model::{CompanyAndRelatedData, ShowCompanyShort};
+use crate::models::company;
+use crate::models::company::member::model::CompanyMemberAndRelatedData;
+use crate::models::company::member::role::model::RoleMemberAndRelatedData;
 use crate::models::company::company_represent::model::CompanyRepresentAndRelatedData;
 use crate::models::company::company_represent::service as company_represent;
-use crate::models::company::model::{CompanyAndRelatedData, ShowCompanyShort};
-use crate::models::company::service as company;
-use crate::models::user;
 
 use async_graphql::{self, Context, Object};
 use uuid::Uuid;
@@ -26,7 +28,7 @@ impl CompanyQuery {
 
         let conn: &PooledConnection = &get_conn(cxt)?;
 
-        company::list::find_companies(
+        company::service::list::find_companies(
             &companies_uuids,
             &logged_user_uuid,
             &crate::models::user::get_set_language(cxt),
@@ -44,7 +46,7 @@ impl CompanyQuery {
 
         let conn: &PooledConnection = &get_conn(cxt)?;
 
-        company::list::find_by_uuid(
+        company::service::list::find_by_uuid(
             &company_uuid,
             &logged_user_uuid,
             &crate::models::user::get_set_language(cxt),
@@ -85,5 +87,45 @@ impl CompanyQuery {
                 "You need to choose a company or a representative company".to_string(),
             )),
         }
+    }
+
+    async fn company_members(
+        &self,
+        cxt: &Context<'_>,
+        company_uuid: Uuid,
+    ) -> ServiceResult<Vec<CompanyMemberAndRelatedData>> {
+        use company::member::service::list::get_by_company_uuid;
+
+        // authorization check
+        let logged_user_uuid = user::get_logged_user_uuid(cxt, true)?;
+
+        let conn: &PooledConnection = &get_conn(cxt)?;
+
+        get_by_company_uuid(
+            &logged_user_uuid,
+            &company_uuid,
+            &crate::models::user::get_set_language(cxt),
+            conn
+        )
+    }
+
+    async fn company_roles(
+        &self,
+        cxt: &Context<'_>,
+        company_uuid: Uuid,
+    ) -> ServiceResult<Vec<RoleMemberAndRelatedData>> {
+        use company::member::role::service::list::get_roles_for_company;
+
+        // authorization check
+        let logged_user_uuid = user::get_logged_user_uuid(cxt, true)?;
+
+        let conn: &PooledConnection = &get_conn(cxt)?;
+
+        get_roles_for_company(
+            &logged_user_uuid,
+            &company_uuid,
+            &crate::models::user::get_set_language(cxt),
+            conn
+        )
     }
 }
