095b944eebad20852db7cb92c143f52f23ed84e3
I_Nosovsky <mananaxep@gmail.com>
2021-10-02 00:38:07 +0300

    make api (add/del) for member, role, access

--- src/graphql/company/company_mutation.rs
+++ src/graphql/company/company_mutation.rs
@@ -1,12 +1,15 @@
-use async_graphql::{self, Context, Object};
-use uuid::Uuid;
-
 use crate::database::{get_conn, PooledConnection};
 use crate::errors::ServiceResult;
+use crate::models::company::access::role_access::model::{IptRoleAccessData, DelRoleAccessData};
 use crate::models::company::certificate::model::IptCompanyCertificateData;
 use crate::models::company::company_represent::model::{IptCompanyRepresentData, SlimCompanyRepresent};
+use crate::models::company::member::model::{IptCompanyMemberData, SlimCompanyMember, DelCompanyMemberData};
+use crate::models::company::member::role::model::{IptRoleMemberData, DelRoleMemberData};
 use crate::models::company::model::{IptCompanyData, SlimCompany};
 use crate::models::relate_ref::file::model::UploadFile;
+
+use async_graphql::{self, Context, Object};
+use uuid::Uuid;
 
 #[derive(Default)]
 pub struct CompanyMutation;
@@ -50,20 +53,16 @@ impl CompanyMutation {
         data: IptCompanyRepresentData,
     ) -> ServiceResult<SlimCompanyRepresent> {
         use crate::models::company::company_represent::service::register::create_company_represent;
-        let conn: &PooledConnection = &get_conn(cxt)?;
 
         let logged_user_uuid = crate::models::user::get_logged_user_uuid(cxt, true)?;
 
-        crate::models::company::util::check_company_access(
+        let conn: &PooledConnection = &get_conn(cxt)?;
+
+        create_company_represent(
             &logged_user_uuid,
-            &data.company_uuid,
-            3,
-            conn,
-        )?;
-
-        crate::models::company::util::check_is_supplier(&data.company_uuid, conn)?;
-
-        create_company_represent(data.into(), conn)
+            &data,
+            conn
+        )
     }
 
     async fn delete_company_represent(
@@ -77,17 +76,119 @@ impl CompanyMutation {
 
         let logged_user_uuid = crate::models::user::get_logged_user_uuid(cxt, true)?;
 
-        crate::models::company::util::check_company_access(
+        delete_company_represent(
             &logged_user_uuid,
             &company_uuid,
-            3,
+            &company_represent_uuid,
             conn,
-        )?;
+        )
+    }
 
-        delete_company_represent(
-            company_uuid,
-            company_represent_uuid,
-            conn,
+    async fn add_company_member(
+        &self,
+        cxt: &Context<'_>,
+        data: IptCompanyMemberData,
+    ) -> ServiceResult<SlimCompanyMember> {
+        use crate::models::company::member::service::add::add_company_member;
+
+        let logged_user_uuid = crate::models::user::get_logged_user_uuid(cxt, true)?;
+
+        let conn: &PooledConnection = &get_conn(cxt)?;
+
+        add_company_member(
+            &logged_user_uuid,
+            &data,
+            conn
+        )
+    }
+
+    async fn delete_company_member(
+        &self,
+        cxt: &Context<'_>,
+        data: DelCompanyMemberData,
+    ) -> ServiceResult<SlimCompanyMember> {
+        use crate::models::company::member::service::delete::del_company_member;
+
+        let logged_user_uuid = crate::models::user::get_logged_user_uuid(cxt, true)?;
+
+        let conn: &PooledConnection = &get_conn(cxt)?;
+
+        del_company_member(
+            &logged_user_uuid,
+            &data,
+            conn
+        )
+    }
+
+    async fn register_company_role(
+        &self,
+        cxt: &Context<'_>,
+        data: IptRoleMemberData,
+    ) -> ServiceResult<i32> {
+        use crate::models::company::member::role::service::register::create_role_member;
+
+        let logged_user_uuid = crate::models::user::get_logged_user_uuid(cxt, true)?;
+
+        let conn: &PooledConnection = &get_conn(cxt)?;
+
+        create_role_member(
+            &logged_user_uuid,
+            &data,
+            conn
+        )
+    }
+
+    async fn delete_company_role(
+        &self,
+        cxt: &Context<'_>,
+        data: DelRoleMemberData,
+    ) -> ServiceResult<i32> {
+        use crate::models::company::member::role::service::delete::del_role_member;
+
+        let logged_user_uuid = crate::models::user::get_logged_user_uuid(cxt, true)?;
+
+        let conn: &PooledConnection = &get_conn(cxt)?;
+
+        del_role_member(
+            &logged_user_uuid,
+            &data,
+            conn
+        )
+    }
+
+    async fn add_access_role(
+        &self,
+        cxt: &Context<'_>,
+        data: IptRoleAccessData,
+    ) -> ServiceResult<bool> {
+        use crate::models::company::access::role_access::service::register::create_role_access;
+
+        let logged_user_uuid = crate::models::user::get_logged_user_uuid(cxt, true)?;
+
+        let conn: &PooledConnection = &get_conn(cxt)?;
+
+        create_role_access(
+            &logged_user_uuid,
+            &data,
+            conn
+        )
+    }
+
+    async fn delete_access_role(
+        &self,
+        cxt: &Context<'_>,
+        data: DelRoleAccessData,
+    ) -> ServiceResult<i32> {
+        use crate::models::company::access::role_access::service::delete::del_role_access;
+
+        let logged_user_uuid = crate::models::user::get_logged_user_uuid(cxt, true)?;
+
+        let conn: &PooledConnection = &get_conn(cxt)?;
+
+        del_role_access(
+            &logged_user_uuid,
+            &data,
+            conn
         )
     }
 }
