8d239c06996f842521d55c06172345ef7d1bb270
I_Nosovsky <mananaxep@gmail.com>
2021-10-02 17:02:26 +0300

    add reference and small code change

--- src/models/component/service/register.rs
+++ src/models/component/service/register.rs
@@ -10,17 +10,22 @@ use diesel::prelude::*;
 use uuid::Uuid;
 
 pub(crate) fn create_component(
-    logged_user_uuid: Uuid,
-    data: IptComponentData,
+    logged_user_uuid: &Uuid,
+    data: &IptComponentData,
     conn: &PgConnection
 ) -> ServiceResult<SlimComponent> {
     use crate::schema::component_ref::dsl::component_ref;
 
+    let parent_component_uuid = match data.parent_component_uuid {
+        Some(x) => x,
+        None => Uuid::parse_str("a5953fd9-7393-4f1e-a899-06b5e159dbf1")?,
+    };
+
     let component_data = ComponentData {
-        parent_component_uuid: data.parent_component_uuid.unwrap_or(Uuid::parse_str("a5953fd9-7393-4f1e-a899-06b5e159dbf1")?),
-        name: data.name,
-        description: data.description,
-        user_uuid: logged_user_uuid,
+        parent_component_uuid,
+        name: data.name.to_string(),
+        description: data.description.to_string(),
+        user_uuid: *logged_user_uuid,
         type_access_id: data.type_access_id,
         component_type_id: data.component_type_id,
         actual_status_id: data.actual_status_id,
@@ -28,6 +33,10 @@ pub(crate) fn create_component(
     };
 
     let component: InsertableComponent = component_data.into();
-    let inserted_component: Component = diesel::insert_into(component_ref).values(&component).get_result(conn)?;
+
+    let inserted_component: Component = diesel::insert_into(component_ref)
+        .values(&component)
+        .get_result(conn)?;
+
     Ok(inserted_component.into())
 }
