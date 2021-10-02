bf5055a3ee3fbea65312dbc0a4986665e858f4d5
I_Nosovsky <mananaxep@gmail.com>
2021-10-02 17:31:04 +0300

    add utilities for get component uuid from modification

---
+++ src/models/component/component_modification/util.rs
@@ -0,0 +1,23 @@
+use crate::errors::{ServiceResult, ServiceError};
+use diesel::prelude::*;
+use uuid::Uuid;
+
+/// Get component uuid from modification by uuid
+pub fn get_component_by_modification(
+    target_modification_uuid: &Uuid,
+    conn: &PgConnection
+) -> ServiceResult<Uuid> {
+    use crate::schema::component_modification_list::dsl::*;
+
+    let get_component_uuid = component_modification_list
+        .filter(uuid.eq(target_modification_uuid))
+        .select(component_uuid)
+        .first::<Uuid>(conn);
+
+    match get_component_uuid {
+        Ok(ct_uuid) => Ok(ct_uuid),
+        _ => Err(ServiceError::BadRequest(
+            "Not found modification data".to_string(),
+        )),
+    }
+}
