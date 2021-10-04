681d6e169b08e0c8d95f069f439455d04be49d6a
mnnxp <mananaxep@gmail.com>
2021-10-04 21:14:37 +0300

    add fn check is base with/without err

--- src/models/component/util.rs
+++ src/models/component/util.rs
@@ -2,8 +2,31 @@ use crate::errors::{ServiceResult, ServiceError};
 use diesel::prelude::*;
 use uuid::Uuid;
 
-/// checking whether the component has flag is_base
+/// Checking whether the component has flag is_base
+/// return true or false
 pub fn check_is_base(
+    target_component_uuid: &Uuid,
+    conn: &PgConnection
+) -> ServiceResult<bool> {
+    use crate::schema::component_ref::dsl::*;
+
+    let get_component_status = component_ref
+        .filter(uuid.eq(target_component_uuid))
+        .select(is_base)
+        .get_result::<bool>(conn);
+
+    match get_component_status {
+        Ok(true) => Ok(true),
+        Ok(false) => Ok(false),
+        _ => Err(ServiceError::BadRequest(
+            "Failed check data".to_string(),
+        )),
+    }
+}
+
+/// Checking whether the component has flag is_base
+/// return err if not base
+pub fn check_is_base_with_err(
     target_component_uuid: &Uuid,
     conn: &PgConnection
 ) -> ServiceResult<bool> {
