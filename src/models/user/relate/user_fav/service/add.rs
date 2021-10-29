540abe40a17665d86f71d3e28b6a73ef616469c4
mnnxp <mananaxep@gmail.com>
2021-10-29 17:31:07 +0300

    add returning false if data is already there

--- src/models/user/relate/user_fav/service/add.rs
+++ src/models/user/relate/user_fav/service/add.rs
@@ -1,9 +1,9 @@
-use crate::errors::{ServiceError, ServiceResult};
+use crate::errors::ServiceResult;
 use crate::models::user::access::util::check_access_user_for_user;
 use crate::models::user::user_fav::model::{
     IptUserFavData, InsertableUserFav
 };
-use crate::schema::user_fav::dsl::*;
+use crate::schema::user_fav::dsl as user_fav;
 use diesel::prelude::*;
 
 pub(crate) fn add_user_fav(
@@ -20,27 +20,42 @@ pub(crate) fn add_user_fav(
         conn
     )?;
 
-    // if have need row, just update is_enabled to true
-    let check_fav = diesel::update(user_fav)
-        .filter(user_favorite_uuid.eq(&data.user_favorite_uuid)
-        .and(user_follower_uuid.eq(&data.user_follower_uuid)))
-        .set(is_enabled.eq(true))
-        .execute(conn)
-        .expect("Failed check fav data");
+    // check active following
+    let check_fav = user_fav::user_fav
+        .filter(user_fav::user_favorite_uuid.eq(&data.user_favorite_uuid)
+        .and(user_fav::user_follower_uuid.eq(&data.user_follower_uuid)))
+        .select(user_fav::is_enabled)
+        .first(conn);
 
     match check_fav {
-        1_usize => Ok(true),
-        0_usize => {
+        Ok(fav) => {
+            if fav {
+                // if data already has
+                Ok(false)
+            } else {
+                // if have need row, just update is_enabled to true
+                diesel::update(user_fav::user_fav)
+                    .filter(user_fav::user_favorite_uuid.eq(&data.user_favorite_uuid)
+                    .and(user_fav::user_follower_uuid.eq(&data.user_follower_uuid)))
+                    .set(user_fav::is_enabled.eq(true))
+                    .execute(conn)
+                    .expect("Failed check fav data");
+
+                Ok(true)
+            }
+        },
+        Err(err) => {
+            debug!("Err check is_enabled: {:?}", err);
+
             // add flag and date created
             let insertable_fav: InsertableUserFav = data.into();
 
-            diesel::insert_into(user_fav)
+            diesel::insert_into(user_fav::user_fav)
                 .values(insertable_fav)
                 .execute(conn)
                 .expect("Failed add fav data");
 
             Ok(true)
         },
-        _ => Err(ServiceError::InternalServerError),
     }
 }
