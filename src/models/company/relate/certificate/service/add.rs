7fffa9536fe3dfc35e35fbf466a6c53ffedf4b1e
mnnxp <mananaxep@gmail.com>
2023-07-30 21:17:41 +0300

    optimize code

--- src/models/company/relate/certificate/service/add.rs
+++ src/models/company/relate/certificate/service/add.rs
@@ -4,9 +4,8 @@ use crate::models::company::certificate::model::{
 };
 use crate::models::company::access::util::check_company_access;
 use crate::models::relate_ref::file::{
-    model::{ListObject, PreliminaryFileData, UploadFile},
+    model::{ListObject, UploadFile},
     service::register::preregister_file,
-    util::get_default_image,
 };
 use crate::storage::model::StorageAccess;
 use crate::storage::presigned_url::upload_presigned_url;
@@ -29,17 +28,10 @@ pub(crate) fn add_certificate(
         conn
     )?;
 
-    // Get data for write information about the file before upload to storage
-    let preliminary_file_data = PreliminaryFileData::from_ipt_file_data(
-        *logged_user_uuid,
-        get_default_image(),
+    let slim_file = preregister_file(
+        logged_user_uuid,
         ListObject::CompanyCertificate(cert_data.company_uuid),
         &cert_data.filename,
-        conn
-    );
-
-    let slim_file = preregister_file(
-        preliminary_file_data,
         conn
     )?;
 
